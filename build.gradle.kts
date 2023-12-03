04affd3095a111c1c4a0d16c63a9b4b68cdb6ffd
shunmame <32508729+shunmame@users.noreply.github.com>
2023-12-03 19:34:16 +0900

    コメント追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+	// spring-initializer
 	id("org.springframework.boot") version "3.2.0"
 	id("io.spring.dependency-management") version "1.1.4"
 	kotlin("jvm") version "1.9.20"
@@ -19,6 +20,7 @@ repositories {
 }
 
 dependencies {
+	// spring-initializer
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
